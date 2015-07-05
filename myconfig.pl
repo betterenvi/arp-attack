4013f09d8045a73f4ee05af63ddefcab404937ea
Jinze Bai <baijinze1994@163.com>
2015-07-05 19:20:17 +0800

    Update myconfig.pl
    add comment

--- myconfig.pl
+++ myconfig.pl
@@ -1,8 +1,10 @@
 #! /usr/bin/perl -w
 use strict;
+# ipv4和硬件地址的提取正则表达式
 my $ip4_reg = qr /\d+(?:\.\d+){3}/;
 my $hwaddr_reg = qr /[\da-fA-f]{2}(?::[\da-fA-f]{2}){5}/;
 
+# 提取解析ifconfig命令的输出
 foreach(split "\n\n", `ifconfig`) {
 	next if /^lo/;
 	next unless /^(\w+)\s+Link\sencap:(\w+)\s+HWaddr\s+($hwaddr_reg)\s+
@@ -13,6 +15,7 @@ foreach(split "\n\n", `ifconfig`) {
 	print " $1 $cidr";
 }
 
+# 通过ip和掩玛获取所在网络的CIDR表示
 sub getCIDR {
 	my ($ip, $mask) = @_;
 	return unless ($ip =~ /$ip4_reg/);
